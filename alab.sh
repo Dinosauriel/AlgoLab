64bc67a8abf40947d6e00eceaca6eee58bdfdbd3
Aurel Feer <aurel.feer@gmail.com>
2022-12-06 12:03:51 +0100

    add world_cup

--- alab.sh
+++ alab.sh
@@ -1,7 +1,8 @@
-
+#!/bin/bash
 PROBLEM=rubeus_hagrid
 TEST=test4
 
+mkdir tmp
 clang++ -std=c++20 $PROBLEM/src/main.cpp -o tmp/a.out
 cat $PROBLEM/public/$TEST.in | tmp/a.out > tmp/run.out
 diff $PROBLEM/public/$TEST.out tmp/run.out
