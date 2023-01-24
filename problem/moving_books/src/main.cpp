44119a9bc6aa617cc9d5f65ae3587fcb266446d7
Aurel Feer <aurel.feer@gmail.com>
2023-01-24 14:47:34 +0100

    solve moving books

--- problem/moving_books/src/main.cpp
+++ problem/moving_books/src/main.cpp
@@ -5,6 +5,16 @@
 #include <climits>
 #include <set>
 
+struct batch {
+  int w;
+  int b;
+  int f;
+};
+
+bool operator <(struct batch const& a, struct batch const& b) {
+  return a.w < b.w;
+}
+
 void test_case() {
   int n;
   int m;
@@ -13,62 +23,57 @@ void test_case() {
   
   std::vector<int> S(n);
   std::vector<int> W(m);
-  std::vector<bool> is_transported(m, false);
-  
+
   for (int i = 0; i < n; ++i) {
     std::cin >> S[i];
   }
-  
+
   for (int i = 0; i < m; ++i) {
     std::cin >> W[i];
   }
-  
-  std::sort(W.begin(), W.end(), std::greater<int>());
-  std::sort(S.begin(), S.end(), std::greater<int>());
+  std::sort(S.begin(), S.end());
+  std::sort(W.begin(), W.end());
 
-  // for (int i = 0; i < m; ++i) {
-  //     std::cout << "W[" << i << "] = " << W[i] << std::endl;
-  // }
-  // for (int i = 0; i < n; ++i) {
-  //   std::cout << "S[" << i << "] = " << S[i] << std::endl;
-  // }
-  
-  if (W[0] > S[0]) {
-    std::cout << "impossible" << std::endl;
-    return;
+  std::vector<struct batch> batches;
+  batches.reserve(n);
+
+  // assign friends to batches
+  for (int i = 0; i < n; ++i) {
+    if (i == 0 || S[i] != S[i - 1]) {
+      batches.push_back({ .w = S[i], .b = 0, .f = 0 });
+    }
+    batches.back().f += 1;
+  }
+
+  // assign boxes to batches
+  size_t b_i = 0;
+  for (int i = 0; i < m; ++i) {
+    while (W[i] > batches[b_i].w) {
+      ++b_i;
+      if (b_i >= batches.size()) {
+        std::cout << "impossible" << std::endl;
+        return;
+      }
+    }
+    batches[b_i].b += 1;
   }
   
-  // greedily take the heaviest box available
-  int minutes = 0;
-  bool boxes_are_available;
-  int frnd;
-  while (true) {
-    boxes_are_available = false;
-    frnd = 0;
-    for (int box = 0; box < m; ++box) {
-      if (is_transported[box]) {
-        continue;
-      }
-      boxes_are_available = true;
-      
-      if (S[frnd] >= W[box]) {
-        // take the box!
-        is_transported[box] = true;
-        ++frnd;
-      }
-      
-      if (frnd >= n) {
-        break;
-      }
-    }
-    if (!boxes_are_available) {
-      break;
-    }
+  long r = 0;
+  long b = 0;
+  long f = 0;
+  
+  for (int i = batches.size() - 1; i >= 0; --i) {
+    f += batches[i].f;
+    long boxes_delivered = r * f;
     
-    minutes += 3;
+    b += batches[i].b;
+    if (b > boxes_delivered) {
+      long rounds_to_complete = ((b - boxes_delivered) + f - 1) / f;
+      r += rounds_to_complete;
+    }
   }
   
-  std::cout << minutes - 1 << std::endl; 
+  std::cout << (r * 3) - 1 << std::endl; 
 }
 
 int main() {
